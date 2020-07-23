500a23490cc18fcb9dad327986434667c416b450
Tomáš Dostál <tomas.dostal.opava@gmail.com>
2020-07-23 09:00:22 +0200

    Update Debugger.h
    Header update + comments

--- Debugger.h
+++ Debugger.h
@@ -1,31 +1,42 @@
-/*
- * Copyright 2011 Alan Burlison, alan@bleaklow.com.  All rights reserved.
- * Use is subject to license terms.
- */
+/**************************************************************************************
+  Description:  ArduinoTaskSheduller Debugger class
+  Type:         Simple Task
+  Purpose:      Universal interface for debugging
 
-/*
- * Simple task classes.
- */
+  Based on: Kevin Gagnon https://github.com/gadgetstogrow/TaskScheduler
+
+  Original copyright: Copyright 2011 Alan Burlison, alan@bleaklow.com.
+  All rights reserved. Use is subject to license terms.
+
+  Modified by: Tomas Dostal <t@xn--dostl-0qa.eu> <t@dostál.eu> https://xn--dostl-0qa.eu
+
+**************************************************************************************/
+
 #include "Task.h"
 
-// ***
-// *** Define the Debugger Class as type Task
-// ***
+// Define the Debugger Class as type Task
 class Debugger : public Task
 {
 public:
-  Debugger();
-  void debugWrite(String debugMsg); //Used for simple debugging of other tasks
-  virtual void run(uint32_t now);   //Override the run() method
-  virtual void run(DateTime dt_now);   //Override the run() method
+    Debugger(); // empty contructor
+    Debugger(int costumBaudRate); // contructor
 
-  virtual void update(uint32_t now);   //Override the run() method
-  virtual void update(DateTime dt_now); 
-  
-  virtual bool canRun(uint32_t now);  //Override the canRun() method
-  virtual bool canRun(DateTime dt_now);  //Override the canRun() method
+    void debugWrite(String debugMsg); // Used for simple debugging of other tasks
+    virtual void run(uint32_t now); // Override the run() method
+    virtual void run(DateTime dt_now); // Override the run() method
 
-  virtual bool canUpdate(uint32_t now) {return false;};  //Override the canRun() method
-  virtual bool canUpdate(DateTime dt_now) {return false;};  //Override the canRun() method
+    virtual void update(uint32_t now); // Override the run() method
+    virtual void update(DateTime dt_now);
 
+    virtual bool canRun(uint32_t now); // Override the canRun() method
+    virtual bool canRun(DateTime dt_now); // Override the canRun() method
+
+    virtual bool canUpdate(uint32_t now)
+    {
+        return false;
+    }; // Override the canRun() method
+    virtual bool canUpdate(DateTime dt_now)
+    {
+        return false;
+    }; // Override the canRun() method
 };
